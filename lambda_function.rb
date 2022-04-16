8daaf997da1f4e00fea21d01817d1cbba8dc890c
Gerardo Mathus <gerardo@nextia.mx>
2022-04-16 13:56:02 -0500

    env vars in lambda

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,15 +1,16 @@
 require "active_record"
 
+#or import models from a file or gem
 class SomeMode < ActiveRecord::Base
 end
 
 def lambda_handler(event:, context:)
   ActiveRecord::Base.establish_connection (
     :adapter  => "postgresql",
-    :host     => "host.com",
-    :username => "user",
-    :password => "password",
-    :database => "database"
+    :host     => ENV["host"],
+    :username => ENV["user"],
+    :password => ENV["password"],
+    :database => ENV["database"]
   )
   { statusCode: 200, body: SomeModel.first }
 end
